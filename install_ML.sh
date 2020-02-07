03d711a9f67a24c73e58097303d692311c4c420f
dspxpert <32154318+dspxpert@users.noreply.github.com>
2020-02-07 10:33:25 +0900

    Create install_ML.sh

---
+++ install_ML.sh
@@ -0,0 +1,10 @@
+sudo pip3 install --upgrade setuptools
+#sudo pip3 install numpy scipy scikit-learn scikit-image pillow h5py matplotlib pandas jupyter opencv-contrib-python tensorflow
+#sudo pip3 install numpy scipy scikit-learn scikit-image pillow h5py matplotlib pandas jupyter opencv-contrib-python tensorflow --upgrade
+
+#PyTorch without CUDA (Windows 10 or Linux)
+#pip install torch==1.4.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+#PyTorch with CUDA 10.1 (Windows 10)
+#pip install torch===1.4.0 torchvision===0.5.0 -f https://download.pytorch.org/whl/torch_stable.html
+#PyTorch with CUDA 10.0 (Linux)
+#pip install torch torchvision
